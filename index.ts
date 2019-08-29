1fd25574ce1750f571e8886e5717411d3088a938
nnqq <43930873+nnqq@users.noreply.github.com>
2019-08-29 22:50:07 +0300

    add exports to index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { hemera } from './hemera';
+import { mongoConnect } from './mongo';
+import { Vk } from './Vk';
+import { logger } from './logger';
+import { handlerDecorator } from './decorators/handlerDecorator';
+
+export { hemera, mongoConnect, Vk, logger, handlerDecorator };
